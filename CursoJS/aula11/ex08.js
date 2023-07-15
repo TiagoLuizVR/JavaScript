a90bb583f384ac72afdb40a8c02745fee6f93926
Tiago Luiz <116216178+TiagoLuizVR@users.noreply.github.com>
2023-07-15 02:28:08 -0400

    condições 1

---
+++ CursoJS/aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`Sua velocidade atual é ${vel}km/h`)
+if(vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade, MULTADO!')
+}
+console.log('Dirija sempre com cuidado! Use o cinto de segurança.')
