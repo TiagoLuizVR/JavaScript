301eaf741f1f070a4671ae9f0f30901b71b6ab4d
Tiago Luiz <116216178+TiagoLuizVR@users.noreply.github.com>
2023-07-16 01:47:40 -0400

    ex hora do dia pronto

--- CursoJS/excondicoes/scripts/script1.js
+++ CursoJS/excondicoes/scripts/script1.js
@@ -3,13 +3,14 @@ var hora = agr.getHours()
 var caixa = document.getElementById('boximg')
 var para = document.getElementsByTagName('p')[0]
 para.innerText = (`Agora são exatamente ${hora} horas`)
-if (hora < 12) {
-    caixa.style.backgroundImage = 'url(imagens/manha.jpg)'
-    caixa.style.backgroundSize = 'cover'
-} else if (hora < 18) {
-    caixa.style.backgroundImage = 'url(imagens/tarde.jpg)'
-    caixa.style.backgroundSize = 'cover'
+
+if (hora < 12){
+    caixa.innerHTML = ('<img src="imagens/manha.jpg" alt="">')
+    document.body.style.backgroundColor = '#c2fafa'
+} else if(hora < 18) {
+    caixa.innerHTML = ('<img src="imagens/tarde.jpg" alt="">')
+    document.body.style.backgroundColor = '#f6c98a'
 } else {
-    caixa.style.backgroundImage = 'url(imagens/noite.jpg)'
-    caixa.style.backgroundSize = 'cover'
+    caixa.innerHTML = ('<img src="imagens/noite.jpg" alt="">')
+    document.body.style.backgroundColor = '#41403f'
 }
