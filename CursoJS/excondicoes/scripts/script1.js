5760a8b7ccd5a7ef1dcaeb73be23310e7225b597
Tiago Luiz <116216178+TiagoLuizVR@users.noreply.github.com>
2023-07-15 22:04:12 -0400

    metade ex 1 condicoes

---
+++ CursoJS/excondicoes/scripts/script1.js
@@ -0,0 +1,17 @@
+var agr = new Date()
+var hora = agr.getHours()
+var caixa = document.getElementById('boximg')
+var para = document.getElementsByTagName('p')[0]
+hora = 12
+para.innerText = (`Agora são exatamente ${hora} horas`)
+
+if (hora < 12) {
+    caixa.style.backgroundImage = 'url(imagens/manha.jpg)'
+    caixa.style.backgroundSize = 'cover'
+} else if (hora < 18) {
+    caixa.style.backgroundImage = 'url(imagens/tarde.jpg)'
+    caixa.style.backgroundSize = 'cover'
+} else {
+    caixa.style.backgroundImage = 'url(imagens/noite.jpg)'
+    caixa.style.backgroundSize = 'cover'
+}
