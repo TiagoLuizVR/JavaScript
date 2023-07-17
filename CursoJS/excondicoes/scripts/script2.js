71f30a92680ec2833e013cb27b4772fa899cd7e2
Tiago Luiz <116216178+TiagoLuizVR@users.noreply.github.com>
2023-07-17 14:20:18 -0400

    projeto finalizado

--- CursoJS/excondicoes/scripts/script2.js
+++ CursoJS/excondicoes/scripts/script2.js
@@ -1,36 +1,32 @@
 function calcular() {
     var agr = new Date()
     var ano = agr.getFullYear()
-    var idade = ano - Number(document.getElementById('iano'). value)
+    var user = Number(document.getElementById('iano'). value)
+    var idade = ano - user
     var caixa = document.getElementById('show')
     var p = document.getElementById('para')
     var sexo = document.querySelector("input[name='sexo']:checked").value
 
-    p.innerHTML = (`Detectamos que você é ${sexo} e tem ${idade} anos`)
-
-    if(sexo == 'homem') {
-        if(idade < 13){
+    if(user >= 1907 && user <= ano) {
+        if(sexo == 'homem' && idade < 13) {
             caixa.innerHTML = ('<img src="imagens/menino.jpg" alt="">')
-        } else if(idade < 19){
+        } else if(sexo == 'homem' && idade < 19) {
             caixa.innerHTML = ('<img src="imagens/garoto.jpg" alt="">')
-        } else if(idade < 60){
+        } else if(sexo == 'homem' && idade < 60) {
             caixa.innerHTML = ('<img src="imagens/homem.jpg" alt="">')
-        }
-        else {
+        } else if(sexo == 'homem' && idade >= 60) {
             caixa.innerHTML = ('<img src="imagens/senhor.jpg" alt="">')
-        }
-    } else if(sexo == 'mulher') {
-        if(idade < 13){
+        } else if(sexo == 'mulher' && idade < 13) {
             caixa.innerHTML = ('<img src="imagens/menina.jpg" alt="">')
-        } else if(idade < 19){
+        } else if(sexo == 'mulher' && idade < 19) {
             caixa.innerHTML = ('<img src="imagens/garota.jpg" alt="">')
-        } else if(idade < 60){
+        } else if(sexo == 'mulher' && idade < 60) {
             caixa.innerHTML = ('<img src="imagens/mulher.jpg" alt="">')
-        }
-        else {
+        } else {
             caixa.innerHTML = ('<img src="imagens/senhora.jpg" alt="">')
         }
+        p.innerHTML = (`Detectamos que você é ${sexo} e tem ${idade} anos`)
     } else {
-        alert = ('Digite os dados corretamente!')
+        alert('Dados incorretos!')
     }
 }
