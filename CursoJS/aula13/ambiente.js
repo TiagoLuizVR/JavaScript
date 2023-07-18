1b127187a5d91e05ca4edcb865d69ec40e761697
Tiago Luiz <116216178+TiagoLuizVR@users.noreply.github.com>
2023-07-17 22:39:24 -0400

    aulas de estruturas de repetições

---
+++ CursoJS/aula13/ambiente.js
@@ -0,0 +1,22 @@
+var i = 1
+
+while(i <= 5) {
+    console.log(`Passo ${i}`)
+    i++
+}
+
+do {
+    console.log(`Passo ${i}`)
+    i++
+} while(i <= 5)
+
+
+
+
+
+
+/*console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')*/
