8897ffb975bcfa9fe980fa3ab91d70e075d560a0
Tiago Luiz <116216178+TiagoLuizVR@users.noreply.github.com>
2023-07-21 18:34:59 -0400

    projeto de contagem finalizado

--- CursoJS/exlacos/scripts/script1.js
+++ CursoJS/exlacos/scripts/script1.js
@@ -5,11 +5,26 @@ function calcular() {
     var caixa = document.getElementById('show')
     var p = document.getElementById('para')
 
-    caixa.innerText = ('')
+    caixa.innerText = ''
 
-    for (var i = inicio; i <= fim ; i += passo){
+    if (document.getElementById('iinicio').value.length == 0 || document.getElementById('ifim').value.length == 0) {
+        alert('Erro!! Preencha todos os campos!')
+    } else if (passo <= 0){
+        alert('Intervalo inválido! Considerando intervalo 1.')
+        passo = 1
+
+        p.innerText = ('Contando...')
+        for (var i = inicio; i <= fim ; i += passo){
         caixa.innerHTML += (`${i} &#x1F449;`)
+        }
+        caixa.innerHTML += ('&#x1F3C1;')
+    } else {
+        p.innerText = ('Contando...')
+        for (var i = inicio; i <= fim ; i += passo){
+        caixa.innerHTML += (`${i} &#x1F449;`)
+        }
+        caixa.innerHTML += ('&#x1F3C1;')
     }
-    caixa.innerHTML += ('&#x1F3C1;')
+
 
 }
