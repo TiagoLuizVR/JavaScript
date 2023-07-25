c546542298d9e93eb21641e6c8e55af7eda76dee
Tiago Luiz <116216178+TiagoLuizVR@users.noreply.github.com>
2023-07-24 23:08:00 -0400

    Melhorias no contador

--- CursoJS/exlacos/scripts/script1.js
+++ CursoJS/exlacos/scripts/script1.js
@@ -5,23 +5,24 @@ function calcular() {
     var caixa = document.getElementById('show')
     var p = document.getElementById('para')
 
-    caixa.innerText = ''
+    caixa.innerHTML = ''
 
-    if (document.getElementById('iinicio').value.length == 0 || document.getElementById('ifim').value.length == 0) {
+    if (document.getElementById('iinicio').value.length == 0 || document.getElementById('ifim').value.length == 0 || document.getElementById('ipasso').value.length == 0) {
         alert('Erro!! Preencha todos os campos!')
-    } else if (passo <= 0){
-        alert('Intervalo inválido! Considerando intervalo 1.')
-        passo = 1
-
-        p.innerText = ('Contando...')
-        for (var i = inicio; i <= fim ; i += passo){
-        caixa.innerHTML += (`${i} &#x1F449;`)
+    }else {
+        if(passo <= 0) {
+            alert('Intervalo inválido! Considerando passo 1.')
+            passo = 1
         }
-        caixa.innerHTML += ('&#x1F3C1;')
-    } else {
-        p.innerText = ('Contando...')
-        for (var i = inicio; i <= fim ; i += passo){
-        caixa.innerHTML += (`${i} &#x1F449;`)
+        p.innerText = 'Contando: '
+        if(inicio <= fim) {
+            for (var i = inicio; i <= fim ; i += passo){
+                caixa.innerHTML += (`${i} &#x1F449;`)
+            }
+        } else {
+            for(var i = inicio; i >= fim; i -= passo){
+                caixa.innerHTML += (`${i} &#x1f449;`)
+            }
         }
         caixa.innerHTML += ('&#x1F3C1;')
     }
