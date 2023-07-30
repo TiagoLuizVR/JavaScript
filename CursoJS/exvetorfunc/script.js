ef555ede8f7c8f82f1ddd863a395674ed1d2c86c
Tiago Luiz <116216178+TiagoLuizVR@users.noreply.github.com>
2023-07-30 03:08:36 -0400

    exercicio funcional

--- CursoJS/exvetorfunc/script.js
+++ CursoJS/exvetorfunc/script.js
@@ -1,14 +1,45 @@
 var numeros = []
-var i = 0
 function adicionar() {
-    var num = document.getElementById('inum').value
+    var num = Number(document.getElementById('inum').value)
     var select = document.getElementById('ilista')
+    var x = 0
 
+    for(var i in numeros) {
+        if(num == numeros[i]) {
+            x = 1
+            break
+        }
+    }
 
-    if(num.length == 0 || num > 100 || num < 0) {
+    if(document.getElementById('inum').value.length == 0 || num > 100 || num < 0 || x == 1) {
         alert('Valor incorreto ou já adicionado!')
-    } else {
+    }else {
         numeros.push(num)
         select.innerHTML += (`<option>Valor ${num} adicionado!</option>`)
     }
+    document.getElementById('inum').value = ''
+}
+
+function finalizar() {
+    var caixa = document.getElementById('show')
+    var maior = numeros[0]
+    var menor = numeros[0]
+    var soma = 0
+
+    for(var ii in numeros) {
+        if(maior < numeros[ii]) {
+            maior = numeros[ii]
+        }
+        if(menor > numeros[ii]) {
+            menor = numeros[ii]
+        }
+        soma += numeros[ii]
+    }
+
+    
+    caixa.innerHTML = (`Foram adicionados ${numeros.length} números!<br>
+    O maior número foi ${maior}<br>
+    O menor número foi ${menor}<br>
+    A soma de todos os números é ${soma}<br>
+    A média de todos os números é ${soma/numeros.length}`)
 }
